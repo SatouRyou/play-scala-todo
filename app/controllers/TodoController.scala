a2ccbcfd1f47875b3ee9fc6e47b1cad7c35c9971
SatouRyou <satouryou1205@gmail.com>
2016-10-12 20:55:13 +0900

    メッセージの表示

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -1,8 +1,10 @@
 package controllers
 
+
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import views._
 
 /**
   * Created by teradashoutarou on 2016/10/12.
@@ -11,5 +13,10 @@ class TodoController @Inject() ( val messagesApi: MessagesApi ) extends Controll
 
   def helloworld = Action { implicit request =>
     Ok("Hello World")
+  }
+
+  def list() = Action { implicit request =>
+    val message: String = "ここにリストを表示"
+    Ok(html.list(message))
   }
 }
