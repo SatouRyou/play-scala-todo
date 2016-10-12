bb626baba02ef540de72496affcbd3363c91f58b
SatouRyou <satouryou1205@gmail.com>
2016-10-12 21:29:29 +0900

    TODO新規登録

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -1,11 +1,12 @@
 package controllers
 
-
+import services.{Todo, TodoService}
 import javax.inject.Inject
 
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
-import services.{Todo, TodoService}
 import views._
 
 /**
@@ -17,8 +18,35 @@ class TodoController @Inject() (todoService: TodoService, val messagesApi: Messa
     Ok("Hello World")
   }
 
+  def create = Action {
+    Ok(html.add(Forms.todoForm))
+  }
+
+  def save() = Action { implicit request =>
+    val todoForm: TodoForm = Forms.todoForm.bindFromRequest().get
+    todoService.insert(Todo(null,todoForm.todo.name))
+    Redirect(routes.TodoController.list())
+  }
+
   def list() = Action { implicit request =>
     val items: Seq[Todo] = todoService.list()
     Ok(html.list(items))
   }
+}
+
+case class TodoForm( command: Option[String], todo: Todo )
+
+object Forms {
+  def todoForm = Form(
+    mapping(
+      "command" -> optional(text),
+      "db" ->
+        mapping(
+          "id" -> optional(longNumber),
+          "name" -> nonEmptyText
+        )
+        ((id, name) => new Todo(id, name))
+        ((n: Todo) => Some((n.id, n.name)))
+    )(TodoForm.apply)(TodoForm.unapply)
+  )
 }
--- app/services/TodoService.scala
+++ app/services/TodoService.scala
@@ -30,4 +30,16 @@ class TodoService @Inject() (dbapi: DBApi) {
       ).as(simple *)
     }
   }
+
+  def insert(todo: Todo) = {
+    db.withConnection { implicit connection =>
+      SQL(
+        """
+      insert into todo values ((select next value for todo_seq), {name})
+        """
+      ).on(
+        'name -> todo.name
+      ).executeUpdate()
+    }
+  }
 }
